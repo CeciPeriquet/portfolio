287d08393d7dd0aefc05b2a9361e7a9ac52f203a
Cecilia Periquet <periwhat@gmail.com>
2023-03-13 12:58:09 +0100

    Update About Me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,38 +1,49 @@
-import '../styles/layout/AboutMe.scss';
-import profilePic from '../images/perfil_ceci_tiny.JPG';
+import "../styles/layout/AboutMe.scss";
+//import profilePic from "../images/perfil_ceci_tiny.JPG";
 function AboutMe(props) {
   return (
     <>
-      <section className='aboutme' id='aboutme'>
-        <h3 className='section_title' id='aboutme'>
+      <section className="aboutme" id="aboutme">
+        <h3 className="section_title left" id="aboutme">
           about me
         </h3>
-        <article className='aboutme-info'>
-          <img src={profilePic} alt='' className='aboutme-img' />
-          <ul className='aboutme-list'>
-            <li className='aboutme-text'>
-              Media professional with more than ten years experience in
-              post-production recently transitioned to web development.
-            </li>
-            <li className='aboutme-text'>
-              Managed, trained and given technical support to different teams
-              both face to face and remote.
-            </li>
-            <li className='aboutme-text'>
-              Prioritized tasks in order to follow companies schedules.
-            </li>
-            <li className='aboutme-text'>
-              I always try to improve workflows and develop new ways of
-              achieving objectives.{' '}
-            </li>
-            <li className='aboutme-text'>
-              Documentation and fluid communication are a must.
-            </li>
-            <li className='aboutme-text'>
-              Looking for an opportunity to grow front-end development skills in
-              an innovative environment.
-            </li>
-          </ul>
+        <article className="aboutme-info">
+          {/* <img src={profilePic} alt="" className="aboutme-img" /> */}
+          <div className="aboutme-img"></div>
+          <div className="aboutme-text-container">
+            <ul className="aboutme-list">
+              <li className="aboutme-text">
+                Media professional with more than ten years experience in
+                post-production recently transitioned to web development.
+              </li>
+              <li className="aboutme-text">
+                Managed, trained and given technical support to different teams
+                both face to face and remote.
+              </li>
+              <li className="aboutme-text">
+                Prioritized tasks in order to follow companies schedules.
+              </li>
+              <li className="aboutme-text">
+                I always try to improve workflows and develop new ways of
+                achieving objectives.{" "}
+              </li>
+              <li className="aboutme-text">
+                Documentation and fluid communication are a must.
+              </li>
+              <li className="aboutme-text">
+                Played in several bands (organized concerts, designed posters
+                and managed social media)
+              </li>
+              <li className="aboutme-text">
+                I currently edit a collaborative fanzine that has led me to give
+                talks and run a successful crowdfunding campaign.
+              </li>
+              <li className="aboutme-text">
+                Looking for an opportunity to grow front-end development skills
+                in an innovative environment.
+              </li>
+            </ul>
+          </div>
         </article>
       </section>
     </>
--- src/data/mySkills.js
+++ src/data/mySkills.js
@@ -1,9 +1,7 @@
 const softSkills = [
-  'Remote Working, mob and pair programming.',
-  'Empathy and effective communication.',
-  'Agile philosophy and Scrum framework.',
-  'Played in several bands (organized concerts, designed posters and managed social media).',
-  'I currently edit a collaborative fanzine that has led me to give talks and run a successful crowdfunding campaign.',
+  "Remote Working, mob and pair programming.",
+  "Empathy and effective communication.",
+  "Agile philosophy and Scrum framework.",
 ];
 
 export default softSkills;
