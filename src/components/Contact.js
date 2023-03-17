b042e3f60f7b29864d7177e727918540e564ef75
Cecilia Periquet <periwhat@gmail.com>
2023-03-17 13:04:54 +0100

    Contact: add form functionality & styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,45 @@
-import '../styles/layout/Contact.scss';
+import "../styles/layout/Contact.scss";
 function Contact(props) {
   return (
     <>
-      <section className='contact' id='contact'>
-        <h3 className='section_title left' id='contact'>
+      <section className="contact" id="contact">
+        <h3 className="section_title left" id="contact">
           contact
         </h3>
+        <div className="contact-container">
+          <div className="contact-image"></div>
+          <div className="form-container">
+            <form
+              action="https://formspree.io/f/xknazlpo"
+              method="POST"
+              className="form"
+            >
+              <label className="form-label">
+                Please don't forget to type your name and email so I can answer
+                you back.
+              </label>
+              <input
+                type="name"
+                name="name"
+                placeholder="Name"
+                className="form-input"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="form-input"
+                required
+              />
+              <label>What can I do for you?</label>
+              <textarea name="message" className="form-textarea"></textarea>
+              <button type="submit" className="form-button">
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
       </section>
     </>
   );
