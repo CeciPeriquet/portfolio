8dbc2c766124799f1f5f26ace974f23de8bff7b2
Cecilia Periquet <periwhat@gmail.com>
2023-03-20 12:18:07 +0100

    About Me: update texts

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,12 +7,21 @@ import playing from "../images/ceci-playing.png";
 import press from "../images/ceci-press.png";
 import posters from "../images/ceci-posters.png";
 import talking from "../images/ceci-talking.png";
+// import { gsap } from "gsap";
+// import { CSSRulePlugin } from "gsap/all";
+// import { useRef, useEffect } from "react";
 
 function AboutMe(props) {
+  /* const aboutMeRef = useRef(null);
+  useEffect(() => {
+    const el = aboutMeRef.current;
+    let tl = gsap.timeline({ defaults: { ease: "power4.inOut", duration: 2 } });
+    gsap.fromTo(el, {}, {});
+  }, []); */
   return (
     <>
       <section className="aboutme left" id="aboutme">
-        <h3 className="section_title" id="aboutme">
+        <h3 className="section_title" id="aboutme" /* ref={aboutMeRef} */>
           about me
         </h3>
 
@@ -25,9 +34,15 @@ function AboutMe(props) {
             />
 
             <p className="aboutme-text">
-              Hi there, I’m Cecilia, a junior front-end developer. In fact,
-              everybody calls me Ceci, only my Mum (when she’s angry at me)
-              calls me Cecilia, but I understand it’s quite difficult to
+              <strong>
+                {" "}
+                Hi there, I’m Cecilia, a junior front-end developer.{" "}
+              </strong>
+              I’m half Spanish, quarter French and quarter English. I was born
+              in Madrid, but I’ve lived in Tenerife, Valencia, London and now
+              I’m in Santander (yep, going to the beach is free and awesome). In
+              fact, everybody calls me Ceci, only my Mum (when she’s angry at
+              me) calls me Cecilia, but I understand it’s quite difficult to
               pronounce Ceci in other languages and even in some parts of Spain…
               so, do as u can.
             </p>
@@ -39,27 +54,30 @@ function AboutMe(props) {
               className="aboutme-img img-right"
             />
             <p className="aboutme-text">
-              I’m half Spanish, quarter French and quarter English. I was born
-              in Madrid, but I’ve lived in Tenerife, Valencia, London and now
-              I’m in Santander (yep, going to the beach is free and awesome). I
-              really need a coffee (or two) first thing in the morning. I’ve got
-              two cats but I’m the kind of person who says hi to every single
-              dog in the streets.
+              {" "}
+              I learnt to speak before I was able to walk, I guess that's why I
+              studied Communication. I’ve got a Bachelor's Degree in Media and
+              Communication, but I've been always passionate by design, so I
+              also made some graphic and web design courses. I really need a
+              coffee (or two) first thing in the morning. I’ve got two cats but
+              I’m the kind of person who says hi to every single dog in the
+              streets.
             </p>
           </li>
+
           <li className="aboutme-list-item-left">
             <img
               src={photographer}
               alt="Me, holding a camera and making pics"
               className="aboutme-img img-left"
             />
+
             <p className="aboutme-text">
-              I’ve got a Bachelor Degree in Media and Communication, and some
-              design courses. I’ve worked in TV for the past few years (making
-              reality shows and sport live coverage events, specially giving
-              technical support on AVID software), but I’ve also been a
-              waitress, a shop assistant, I’ve worked in marketing agencies, and
-              I even appear in an Almodovar movie 🤷🏻‍♀️.
+              I’ve worked in TV for the past few years (making reality shows and
+              sport live coverage events, specially giving technical support on
+              AVID software), but I’ve also worked in marketing agencies, been a
+              waitress, a shop assistant and I even appear in an Almodovar movie
+              🤷🏻‍♀️.
             </p>
           </li>
 
@@ -103,19 +121,21 @@ function AboutMe(props) {
               className="aboutme-img img-left"
             />
             <p className="aboutme-text">
-              Right now I edit a collaborative zine, that allows me to meet and
-              connect writers and illustrators (and I also write and draw
-              myself). It’s a really cool project, that has given me the
-              opportunity to give talks and run a successful crowdfunding
-              campaign. Oh, I forgot to say that I learnt to speak before I was
-              able to walk.
+              Right now I layout, edit and self-publish a collaborative zine,
+              that allows me to meet and connect writers and illustrators (and I
+              also write and draw myself). It’s a really cool project, that has
+              given me the opportunity to give talks and run a successful
+              crowdfunding campaign. (I told you, I learnt to speak before I was
+              able to walk).
             </p>
           </li>
           <li className="aboutme-list-item-solo">
             <p className="aboutme-text">
-              Anyway, I just finished a Web Development Bootcamp and I’m whiling
-              to find a company to keep learning and growing as a front-end
-              developer and share all my previous knowledge.
+              Anyway, I'm a newbie in Web Development but I'm happy to be a part
+              of such a special, creative and supportive community. Now I just
+              need to find a company that allows me to keep learning and growing
+              as a front-end developer and share all my previous knowledge and
+              skills. Might be yours?
             </p>
           </li>
         </ul>
